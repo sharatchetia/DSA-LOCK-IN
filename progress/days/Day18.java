f74b07b2a3e1bfa68464e919c139b3c4007e5d90
Sharat Chandra Chetia <sharatchetia80@gmail.com>
2026-07-02 22:30:01 +0530

    Update Day18.java: refine snippets and comments

--- progress/days/Day18.java
+++ progress/days/Day18.java
@@ -121,3 +121,38 @@
 //         return true;
 //     }
 // }
+
+
+
+
+// leetcode 141 (same almost done by myself using newly learned slow and fast pointers)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         if(head == null || head.next == null){
+//             return false;
+//         }
+
+//         ListNode slow = head;
+//         ListNode fast = head;
+
+//         while(fast != null && fast.next != null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//             if(slow == fast){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
